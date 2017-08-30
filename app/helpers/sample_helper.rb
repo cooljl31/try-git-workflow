9e968ae9c4733d0f75d833240782ea00d8d96066
jim lesperance <contact@jimlesperance.com>
2017-08-30 16:53:07 +0200

    add index

---
+++ app/helpers/sample_helper.rb
@@ -0,0 +1,13 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module GitflowTest
+  class App
+    module SampleHelper
+      # def simple_helper_method
+      # ...
+      # end
+    end
+
+    helpers SampleHelper
+  end
+end
